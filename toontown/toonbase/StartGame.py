1c15fb4d840dfa189e1359023e2045364bc26b29
Gray <Michael.Gray@EUDSDODEA>
2017-11-07 14:19:38 +0100

    traceback stuff for when program crashes

--- toontown/toonbase/StartGame.py
+++ toontown/toonbase/StartGame.py
@@ -5,3 +5,8 @@ import traceback
 try:
     run()
 except:
+    traceback_info = traceback.format_exc()
+else:
+    traceback_info = 'Program crashed but no error was found'
+finally:
+    print(traceback_info)
