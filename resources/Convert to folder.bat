c49c592adb06f7bcca8da3e87b160b705b6ac55a
Gray <Michael.Gray@EUDSDODEA>
2017-11-07 14:31:31 +0100

    add phase files

---
+++ resources/Convert to folder.bat
@@ -0,0 +1,16 @@
+@echo off
+C:\Panda3d-1.10.0\bin\multify -x -f phase_3.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_3.5.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_4.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_5.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_5.5.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_6.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_7.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_8.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_9.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_10.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_11.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_12.mf
+C:\Panda3d-1.10.0\bin\multify -x -f phase_13.mf
+echo CONVERTING COMPLETE
+pause
--- start-game.bat
+++ start-game.bat
@@ -1,1 +1,6 @@
-Panda3D-1.10.0-x64\python\ppython main.py
+@echo off
+title Toontown Single Player 
+:main
+"Panda3D-1.10.0-x64/python/ppython" main.py
+pause
+goto main
